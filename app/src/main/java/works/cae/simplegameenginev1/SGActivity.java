5b2386fac8d52c248b960675d4465245a35b738d
Cae Goncalves <hello@cae.works>
2016-12-18 00:04:31 -0200

    Incluídos métodos de preparação da tela para rodar em fullscreen

--- app/src/main/java/works/cae/simplegameenginev1/SGActivity.java
+++ app/src/main/java/works/cae/simplegameenginev1/SGActivity.java
@@ -1,9 +1,19 @@
 package works.cae.simplegameenginev1;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.nfc.Tag;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import static android.R.attr.breadCrumbShortTitle;
+import static android.R.attr.orientation;
+import static works.cae.simplegameenginev1.SGActivity.SGOrientation.LANDSCAPE;
+import static works.cae.simplegameenginev1.SGActivity.SGOrientation.PORTRAIT;
 
 /**
  * Created by salfl on 16/12/16.
@@ -13,10 +23,14 @@ public class SGActivity extends Activity {
 
     public static final String TAG = "SimpleGameEngine";
 
+    public enum SGOrientation {
+        LANDSCAPE,
+        PORTRAIT
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
@@ -53,6 +67,33 @@ public class SGActivity extends Activity {
     protected void onStop() {
         super.onStop();
         Log.d(TAG, "onStop( chamado");
+    }
+
+    public void enableKeepScreenOn() {
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
+
+    public void setOrientation(SGOrientation) {
+        switch(orientation) {
+            case LANDSCAPE:
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                break;
+
+            case PORTRAIT:
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                break;
+        }
+    }
+
+    public void enableFullScreen() {
+        Window window = getWindow();
+        window.addFlags((WindowManager.LayoutParams.FLAG_FULLSCREEN));
+        window.requestFeature(Window.FEATURE_NO_TITLE);
+
+        if(Build.VERSION.SDK_INT >= 19) {
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
     }
 
 }
